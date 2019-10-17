952f30265cd2e80548328963a8e3791b8f5e63ef
renatosrib <renatosrib@gmail.com>
2019-10-16 23:59:02 -0300

    Adiciona endpoint com parametros de url

--- src/enutrof_chest/core.clj
+++ src/enutrof_chest/core.clj
@@ -10,13 +10,14 @@
 
 (defn calculadora-basica
   "Resolve calculos básicos"
-  [requisiçao]
-  (let [op1 (Integer.(get-in requisiçao [:route-params :op1]))
-        op2 (Integer. (get-in requisiçao [:route-params :op2]))
-        op (get-in requisiçao [:route-params :op])
+  [requisicao]
+  (println requisicao)
+  (let [op1 (Integer. (get-in requisicao [:route-params :op1]))
+        op2 (Integer. (get-in requisicao [:route-params :op2]))
+        op (get-in requisicao [:route-params :op])
         f (get operandos op)]
-    {:status 200
-     :body (f op1 op2)
+    {:status  200
+     :body    (str (f op1 op2))
      :headers {}}))
 
 
@@ -28,16 +29,16 @@
 
 (defn request-info
   "Retorna informações sobre a requisição"
-  [request] {:status 200
-             :body (pr-str request)
+  [request] {:status  200
+             :body    (pr-str request)
              :headers {}})
 
 (defn bem-vindo
   "Exibe mensagem de bem vindo com o nome do usuário, informado via url"
   [request]
-  {:status 200
-   :body (let [nome (get-in request [:route-params :nome])]
-           (str "Seja bem vindo Sr." nome))
+  {:status  200
+   :body    (let [nome (get-in request [:route-params :nome])]
+              (str "Seja bem vindo Sr." nome))
 
    :headers {}})
 
@@ -46,7 +47,7 @@
            (GET "/request-info" [] request-info)
            (GET "/:nome/bem-vindo" [] bem-vindo)
            (GET "/:op1/:op/:op2/calculadora-basica" [] calculadora-basica)
-           (not-found"<h1>A página não existe</h1>"))
+           (not-found "<h1>A página não existe</h1>"))
 
 
 ;(defn -main [port-number]
